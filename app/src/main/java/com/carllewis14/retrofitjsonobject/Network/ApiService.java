46248490da3b57e824c2a521de5f1871fb3dea41
carl Lewis <carl.lewis@incisivemedia.com>
2017-02-17 17:06:12 +0000

    updated the Retroclient and Apiservice classes

---
+++ app/src/main/java/com/carllewis14/retrofitjsonobject/Network/ApiService.java
@@ -0,0 +1,8 @@
+package com.carllewis14.retrofitjsonobject.Network;
+
+/**
+ * Created by carllewis on 17/02/2017.
+ */
+
+public interface ApiService {
+}
--- app/src/main/java/com/carllewis14/retrofitjsonobject/Network/RetroClient.java
+++ app/src/main/java/com/carllewis14/retrofitjsonobject/Network/RetroClient.java
@@ -1,8 +1,34 @@
 package com.carllewis14.retrofitjsonobject.Network;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /**
  * Created by carllewis on 17/02/2017.
  */
 
 public class RetroClient {
+
+    /**
+     * URL
+     */
+
+    private static String url = "http://pratikbutani.x10.mx/";
+
+    /**
+     * Retrofit instance - builds url l
+     */
+
+    private static Retrofit getRetrofitInstance() {
+        return new Retrofit.Builder()
+                .baseUrl(url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+
+    public static ApiService getApiService(){
+        return getRetrofitInstance().create(ApiService.class);
+    }
+
 }
